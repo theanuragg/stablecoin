b57aa25ccb38c6d2ac9caa048dca754567efe8b1
anurag <anurags9693@gmail.com>
2025-12-15 00:30:33 +0530

    fix build error  in deposit

--- programs/stablecoin/src/instruction/deposit/deposit_collateral_and_mint_token.rs
+++ programs/stablecoin/src/instruction/deposit/deposit_collateral_and_mint_token.rs
@@ -1,96 +1,108 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::spl_associated_token_account::solana_program::example_mocks::solana_account::Account, token_interface::{Mint, Token2022, TokenAccount, }};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, Token2022, TokenAccount}
+};
 use pyth_solana_receiver_sdk::price_update::PriceUpdateV2;
 
-use crate::{Collateral, Config, SEED_COLLATERAL_ACCOUNT, SEED_CONFIG_ACCOUNT, SEED_SOL_ACCOUNT, instruction::{check_health_factor, desposit_sol, mint_tokens}};
-
-
+use crate::{
+    Collateral, 
+    Config, 
+    SEED_COLLATERAL_ACCOUNT, 
+    SEED_CONFIG_ACCOUNT, 
+    SEED_SOL_ACCOUNT,
+    instruction::{check_health_factor, deposit_sol, mint_tokens}
+};
 
 #[derive(Accounts)]
 pub struct DepositCollateralAndMintToken<'info> {
-
     #[account(mut)]
     pub depositor: Signer<'info>,
 
     #[account(
-        seeds =[SEED_CONFIG_ACCOUNT],
-        bump= config_account.bump,
+        seeds = [SEED_CONFIG_ACCOUNT],
+        bump = config_account.bump,
         has_one = mint_account,
-
     )]
     pub config_account: Box<Account<'info, Config>>,
 
     #[account(mut)]
     pub mint_account: InterfaceAccount<'info, Mint>,
+    
     #[account(
         init_if_needed,
         payer = depositor,
-        space= 8 + Collateral::INIT_SPACE,
+        space = 8 + Collateral::INIT_SPACE,
         seeds = [SEED_COLLATERAL_ACCOUNT, depositor.key().as_ref()],
         bump,
     )]
     pub collateral_account: Account<'info, Collateral>,
+    
+    /// CHECK: This is a PDA used to hold SOL collateral
     #[account(
         mut,
-        seeds = [SEED_COLLATERAL_ACCOUNT, depositor.key().as_ref()],
+        seeds = [SEED_SOL_ACCOUNT, depositor.key().as_ref()],
         bump,
     )]
     pub sol_account: SystemAccount<'info>,
+    
     #[account(
         init_if_needed,
         payer = depositor,
-        associated_token::mint =mint_account,
+        associated_token::mint = mint_account,
         associated_token::authority = depositor,
         associated_token::token_program = token_program,
     )]
     pub token_account: InterfaceAccount<'info, TokenAccount>,
+    
     pub token_program: Program<'info, Token2022>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-
     pub system_program: Program<'info, System>,
-    pub price_update: Account<'info, PriceUpdate>,
-    
-
+    pub price_update: Account<'info, PriceUpdateV2>,
 }
-
 
 pub fn process_deposit_collateral_and_mint_token(
     ctx: Context<DepositCollateralAndMintToken>,
+    amount_collateral: u64,
     amount_to_mint: u64,
-    amount_collateral: u64,   
 ) -> Result<()> {
+    let collateral_account = &mut ctx.accounts.collateral_account;
     
+    // Update collateral account state FIRST (before health check)
+    collateral_account.lamport_amount += amount_collateral;
+    collateral_account.amount_minted += amount_to_mint;
 
-    let Collateral_account: &mut Account<'info, Collateral> = &mut ctx.accounts.collateral_account;
-    Collateral_account.lamports_balance += amount_collateral;
-    Collateral_account.amount_minted += amount_to_mint;
-
-    if !Collateral_account.is_initialized {
-        Collateral_account.depositor = ctx.accounts.depositor.key();
-        Collateral_account.sol_amount = ctx.accounts.sol_account.key();
-        Collateral_account.token_amount = ctx.accounts.token_account.key();
-        Collateral_account.bump = ctx.bumps.collateral_account;
-        Collateral_account.is_initialized = true;
+    if !collateral_account.is_initialized {
+        collateral_account.depositor = ctx.accounts.depositor.key();
+        collateral_account.sol_amount = ctx.accounts.sol_account.key();
+        collateral_account.token_amount = ctx.accounts.token_account.key();
+        collateral_account.bump = ctx.bumps.collateral_account;
+        collateral_account.is_initialized = true;
     }
 
+    // Check health factor AFTER updating the state
     check_health_factor(
-        &ctx.accounts.collateral_account,
+        collateral_account,  // Don't use &ctx.accounts here since we already have the reference
         &ctx.accounts.config_account, 
-        &ctx.accounts.price_update)?;
+        &ctx.accounts.price_update
+    )?;
 
+    // Deposit SOL
     deposit_sol(
         &ctx.accounts.depositor,
         &ctx.accounts.sol_account,
-        ctx.accounts.system_program,
-        amount_collateral,
-    )?; 
+        &ctx.accounts.system_program,
+        amount_collateral,  // Use the parameter directly
+    )?;
 
+    // Mint tokens
     mint_tokens(
         &ctx.accounts.mint_account,
         &ctx.accounts.token_account,
         &ctx.accounts.token_program,
-        amount_to_mint,
+        amount_to_mint,  // Use the parameter directly
         ctx.accounts.config_account.bump_mint_account,
-        )?;
+    )?;
+    
     Ok(())
 }
--- programs/stablecoin/src/instruction/deposit/utils.rs
+++ programs/stablecoin/src/instruction/deposit/utils.rs
@@ -1,23 +1,29 @@
-use anchor_lang::{accounts::signer, prelude::*, system_program::{Transfer, transfer}};
-use anchor_spl::{token_2022::{mint_to, MintTo, Token2022},token_interface::{Mint, TokenAccount}};
+use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
+use anchor_spl::{
+    token_2022::{mint_to, MintTo},
+    token_interface::{Mint, TokenAccount, Token2022}
+};
+
+use crate::SEED_MINT_ACCOUNT;
 
 pub fn mint_tokens<'info>(
-    mint_account: &Interface<'info, Mint>,
+    mint_account: &InterfaceAccount<'info, Mint>,
     token_account: &InterfaceAccount<'info, TokenAccount>,
-    token_program: Program<'info, Token2022>,
+    token_program: &Program<'info, Token2022>,
     amount: u64,
     bump: u8,
-) -> Result<()>{
-
-    let signer_seeds: &[&[u8]] = &[
-        SEED_MINT_ACCOUNT,
-        &[bump],
+) -> Result<()> {
+    let signer_seeds: &[&[&[u8]]] = &[
+        &[
+            SEED_MINT_ACCOUNT,
+            &[bump],
+        ]
     ];
 
     mint_to(
         CpiContext::new_with_signer(
-            program::token_program.to_account_info(),
-            MintTo{
+            token_program.to_account_info(),
+            MintTo {
                 mint: mint_account.to_account_info(), 
                 to: token_account.to_account_info(),
                 authority: mint_account.to_account_info(),
@@ -25,20 +31,25 @@ pub fn mint_tokens<'info>(
             signer_seeds,
         ),
         amount,
-    );
+    )?;
+    
+    Ok(())
 }
 
-
-pub fn  desposit_sol<'info>(
-    from; &signer<'info>,
+pub fn deposit_sol<'info>(
+    from: &Signer<'info>,
     to: &SystemAccount<'info>,
-    system_program: Program<'info, System>,
-    
+    system_program: &Program<'info, System>,
+    amount: u64,
 ) -> Result<()> {
-   transfer(CpiContext::new(program: system_program.to_account_info(), 
-   accounts: Transfer{
-    from: from.to_account_info(),
-    to: to.to_account_info(),
-   },
-), 
+    transfer(
+        CpiContext::new(
+            system_program.to_account_info(),
+            Transfer {
+                from: from.to_account_info(),
+                to: to.to_account_info(),
+            },
+        ),
+        amount,
+    )
 }
